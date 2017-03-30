9861fec1cc900ef5dc4afee8e0ca6e0cba91ee5b
Sanwer <sanwer@qq.com>
2017-03-30 19:58:53 +0800

    Init App

---
+++ Resource.h
@@ -0,0 +1,9 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by iHelper.rc
+//
+
+#define IDI_IHELPER				101
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
---
+++ iHelper.cpp
@@ -0,0 +1,16 @@
+// iHelper.cpp : 定义应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "iHelper.h"
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	return 0;
+}
